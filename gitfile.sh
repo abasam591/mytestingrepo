2fa1c8196289afd8c4ac30a32662bab7ca383a1e
root <root@ip-172-16-1-153.ec2.internal>
2020-03-19 08:57:41 +0000

    commit -4

--- gitfile.sh
+++ gitfile.sh
@@ -4,8 +4,9 @@ echo {a..z}
 read -p"lets create new user:" USERNAME
 echo $USERNAME
 useradd -m $USERNAME
-VALUE=$( cat /etc/passwd | grep -i "$USERNAME" | cut -d ":" -f1 )
-echo "user is $USERNAME && $VALUE"
+VALUE=$( cat /etc/passwd | grep -i "$USERNAME" )
+echo "user is $USERNAME"
+echo "user details: $VALUE"
 
 
 
